8707af03d65b41f6233ea95dca227aaf9195b906
Akram <ak4am.mo@gmail.com>
2018-09-21 21:21:20 +0900

    Add api routes for each CRUD operations

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,24 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('product', 'ProductController@create');
+Route::get('product/{id}', 'ProductController@read');
+Route::update('product/{id}', 'ProductController@update');
+Route::delete('product/{id}', 'ProductController@delete');
+
+Route::post('lineItem', 'LineItemController@create');
+Route::get('lineItem/{id}', 'ProductController@read');
+Route::update('lineItem/{id}', 'ProductController@update');
+Route::delete('lineItem/{id}', 'ProductController@delete');
+
+Route::post('shop', 'ShopController@create');
+Route::get('shop/{id}', 'ShopController@read');
+Route::update('shop/{id}', 'ShopController@update');
+Route::delete('shop{id}', 'ShopController@delete');
+
+Route::post('order', 'OrderController@create');
+Route::get('order/{id}', 'OrderController@read');
+Route::post('order/update/{id}', 'OrderController@updateOrderTotal');
+Route::update('order/{id}', 'OrderController@update');
+Route::delete('order/{id}', 'OrderController@delete');
