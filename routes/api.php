c569272d6ece0f5d4137941a26239d7edb08e135
Akram <ak4am.mo@gmail.com>
2018-09-23 15:39:44 +0900

    Add comment to api Routes

--- routes/api.php
+++ routes/api.php
@@ -17,23 +17,26 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+//Product CRUD Operations routes
 Route::post('product', 'ProductController@create');
 Route::get('product/{id}', 'ProductController@read');
 Route::patch('product/{id}', 'ProductController@update');
 Route::delete('product/{id}', 'ProductController@delete');
 
+//LineItem CRUD Operations routes
 Route::post('lineItem', 'LineItemController@create');
 Route::get('lineItem/{id}', 'LineItemController@read');
 Route::patch('lineItem/{id}', 'LineItemController@update');
 Route::delete('lineItem/{id}', 'LineItemController@delete');
 
+//Shop CRUD Operations routes
 Route::post('shop', 'ShopController@create');
 Route::get('shop/{id}', 'ShopController@read');
 Route::get('shop/order/{id}', 'ShopController@readShopOrder');
 Route::patch('shop/{id}', 'ShopController@update');
 Route::delete('shop{id}', 'ShopController@delete');
 
+//Orders CRUD Operations routes
 Route::post('order', 'OrderController@create');
 Route::get('order/{id}', 'OrderController@read');
 Route::patch('order/update/{id}', 'OrderController@updateOrderTotal');
